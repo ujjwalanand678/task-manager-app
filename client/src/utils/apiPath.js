38ccb8f9b4ba343387c85906b522888a88e20f13
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-12-19 00:50:53 +0530

    api paths contructed for frontend

---
+++ client/src/utils/apiPath.js
@@ -0,0 +1,43 @@
+export const BASE_URL = "http://localhost:8000";
+
+// utils/apiPaths.js
+export const API_PATHS = {
+  AUTH: {
+    REGISTER: "/api/auth/register", // Register a new user (Admin or Member)
+    LOGIN: "/api/auth/login", // Authenticate user & return JWT token
+    GET_PROFILE: "/api/auth/profile", // Get logged-in user details
+  },
+
+  USERS: {
+    GET_ALL_USERS: "/api/users", // Get all users (Admin only)
+    GET_USER_BY_ID: (userId) => `/api/users/${userId}`, // Get user by ID
+    CREATE_USER: "/api/users", // Create a new user (Admin only)
+    UPDATE_USER: (userId) => `/api/users/${userId}`, // Update user details
+    DELETE_USER: (userId) => `/api/users/${userId}`, // Delete a user
+  },
+
+  TASKS: {
+    GET_DASHBOARD_DATA: "/api/tasks/dashboard-data", // Admin dashboard
+    GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data", // User dashboard
+
+    GET_ALL_TASKS: "/api/tasks", // Admin: all | User: assigned only
+    GET_TASK_BY_ID: (taskId) => `/api/tasks/${taskId}`, // Get task by ID
+    CREATE_TASK: "/api/tasks", // Create a new task (Admin only)
+    UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`, // Update task details
+    DELETE_TASK: (taskId) => `/api/tasks/${taskId}`, // Delete task (Admin only)
+
+    UPDATE_TASK_STATUS: (taskId) =>
+      `/api/tasks/${taskId}/status`, // Update task status
+    UPDATE_TODO_CHECKLIST: (taskId) =>
+      `/api/tasks/${taskId}/todo`, // Update todo checklist
+  },
+
+  REPORTS: {
+    EXPORT_TASKS: "/api/reports/export/tasks", // Download all tasks (Excel)
+    EXPORT_USERS: "/api/reports/export/users", // Download user-task report
+  },
+
+  IMAGE: {
+    UPLOAD_IMAGE: "/api/auth/upload-image", // Upload profile image
+  },
+};
