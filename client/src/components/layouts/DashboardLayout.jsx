e22a93bd0a15d95052a16184aca2423d771d5ae3
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-12-20 00:21:19 +0530

    task list , dashboard , side menu , helper.js

--- client/src/components/layouts/DashboardLayout.jsx
+++ client/src/components/layouts/DashboardLayout.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/UserContext.jsx";
+import Navbar from "./Navbar.jsx";
+import SideMenu from "./SideMenu.jsx";
 
 const DashboardLayout = ({ children, activeMenu }) => {
   const { user } = useContext(UserContext);
