8bb7b7e4d3fb2dca0f80fa0075eff8f3caa47154
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-12-20 01:02:01 +0530

    create task for dashboard

--- client/src/pages/Admin/CreateTask.jsx
+++ client/src/pages/Admin/CreateTask.jsx
@@ -1,7 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { PRIORITY_DATA } from "../../utils/data";
+import axiosInstance from "../../utils/axiosInstance.js";
+import { API_PATHS } from "../../utils/apiPath.js";
+import toast from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import moment from "moment";
+import { LuTrash2 } from "react-icons/lu";
 
 const CreateTask = () => {
-  return <div>CreateTask</div>;
+  const location = useLocation();
+  const { taskId } = location.state || {};
+  const navigate = useNavigate();
+
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "",
+    priority: "Low",
+    dueDate: null,
+    assignedTo: [],
+    todoChecklist: [],
+    attachments: [],
+  });
+
+  const [currentTask, setCurrentTask] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
+
+  const handleValueChange = (key, value) => {
+    setTaskData((prevData) => ({
+      ...prevData,
+      [key]: value,
+    }));
+  };
+
+  const clearData = () => {
+    setTaskData({
+      title: "",
+      description: "",
+      priority: "Low",
+      dueDate: null,
+      assignedTo: [],
+      todoChecklist: [],
+      attachments: [],
+    });
+  };
+
+  // Create Task
+  const createTask = async () => {};
+
+  // Update Task
+  const updateTask = async () => {};
+
+  const handleSubmit = async () => {};
+
+  // Get task info by ID
+  const getTaskDetailsByID = async () => {};
+
+  // Delete Task
+  const deleteTask = async () => {};
+
+  return (
+    <DashboardLayout activeMenu="Create Task">
+      <div className="mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="form-card col-span-3">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl md:text-2xl font-medium">
+                {taskId ? "Update Task" : "Create Task"}
+              </h2>
+
+              {taskId && (
+                <button
+                  className="flex items-center gap-1.5 text-[13px] font-medium"
+                  onClick={() => setOpenDeleteAlert(true)}
+                >
+                  <LuTrash2 /> Delete
+                </button>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label>Task Title</label>
+              <input
+                placeholder="Create App UI"
+                value={taskData.title}
+                onChange={({ target }) =>
+                  handleValueChange("title", target.value)
+                }
+              />
+            </div>
+            <div className="mt-3">
+              <label className="text-xs font-medium text-slate-600">
+                Description
+              </label>
+
+              <textarea
+                placeholder="Describe task"
+                className="form-input"
+                rows={4}
+                value={taskData.description}
+                onChange={({ target }) =>
+                  handleValueChange("description", target.value)
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default CreateTask;
